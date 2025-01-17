8d61e0fa70bf3aa02dbec4e7c9ef638e8f10e32d
sahityaakula9 <137136572+sahityaakula9@users.noreply.github.com>
2025-01-16 20:19:16 -0600

    added namithas first name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("Sahitya ");
             System.out.println("my name is namitha");
+             System.out.println("my name is Jammula ");
         }
     }
 }
