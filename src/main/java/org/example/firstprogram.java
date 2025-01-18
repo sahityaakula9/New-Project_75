c21fe5c64177577074b11ef94150f5acc1d39a2e
sahityaakula9 <137136572+sahityaakula9@users.noreply.github.com>
2025-01-18 17:24:18 -0600

    added developer

--- src/main/java/org/example/firstprogram.java
+++ src/main/java/org/example/firstprogram.java
@@ -3,5 +3,6 @@ package org.example;
 public class firstprogram {
     public static void main(String[] args) {
         System.out.println("Hello Sahitya");
+        System.out.println("I am a developer");
     }
 }
